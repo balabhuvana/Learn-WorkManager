40505abf0c0ef6efef40710060415ae3ec292e40
Balamurugan Selvam <balamurugan.selvam@hcl.com>
2020-04-22 09:39:46 +0530

    Observing the work status.

--- app/src/main/java/com/example/myapplication/WorkManagerFragment.kt
+++ app/src/main/java/com/example/myapplication/WorkManagerFragment.kt
@@ -2,10 +2,12 @@ package com.example.workmanager
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.work.*
 import com.example.myapplication.R
 import kotlinx.android.synthetic.main.fragment_work_manager.*
@@ -15,6 +17,8 @@ import java.util.concurrent.TimeUnit
  * A simple [Fragment] subclass.
  */
 class WorkManagerFragment : Fragment() {
+
+    lateinit var uploadWorkRequest: OneTimeWorkRequest;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,18 +39,39 @@ class WorkManagerFragment : Fragment() {
 
             val data = workDataOf(USER_NAME to "Arunkumar")
 
-            val uploadWorkRequest = OneTimeWorkRequestBuilder<SimpleWorker>().setConstraints(constraints)
+            uploadWorkRequest = OneTimeWorkRequestBuilder<SimpleWorker>().setConstraints(constraints)
                 .setBackoffCriteria(BackoffPolicy.LINEAR, OneTimeWorkRequest.MIN_BACKOFF_MILLIS, TimeUnit.MILLISECONDS)
                 .setInputData(data)
                 .addTag(SIMPLE_TAG)
                 .build()
             WorkManager.getInstance(this!!.context!!).enqueue(uploadWorkRequest)
+
+            observeSimpleWorkStatus()
         }
+    }
+
+    fun observeSimpleWorkStatus() {
+        WorkManager.getInstance(this!!.context!!).getWorkInfoByIdLiveData(uploadWorkRequest.id)
+            .observe(viewLifecycleOwner,
+                Observer { workInfo ->
+                    if (workInfo != null) {
+                        if (workInfo.state == WorkInfo.State.BLOCKED) {
+                            Log.i(TAG, "${WorkInfo.State.BLOCKED}")
+                        } else if (workInfo.state == WorkInfo.State.ENQUEUED) {
+                            Log.i(TAG, "${WorkInfo.State.ENQUEUED}")
+                        } else if (workInfo.state == WorkInfo.State.RUNNING) {
+                            Log.i(TAG, "${WorkInfo.State.RUNNING}")
+                        } else if (workInfo.state == WorkInfo.State.SUCCEEDED) {
+                            Log.i(TAG, "${WorkInfo.State.SUCCEEDED}")
+                        }
+                    }
+                })
     }
 
     companion object {
         const val USER_NAME = "username"
         const val SIMPLE_TAG = "simple_tag"
+        val TAG = WorkManagerFragment::class.java.simpleName
     }
 
 }
